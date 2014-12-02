86fe5614af9e2a51dee6ee65aa3c9f7be75b09e1
Tshepo <tramats@thoughtworks.com>
2014-12-02 11:22:10 +0200

    stuffs

--- src/calendar/GoogleCalendarTestThingy.java
+++ src/calendar/GoogleCalendarTestThingy.java
@@ -27,8 +27,8 @@ public class GoogleCalendarTestThingy {
         JsonFactory jsonFactory = new JacksonFactory();
 
         Set<String> scope = Collections.singleton(CalendarScopes.CALENDAR);
-        String clientId = "764517824387-u2fs564hm8ag8o8sj03r1l1g13di3rrj.apps.googleusercontent.com";
-        String clientSecret = "kYDYMXAttTEZqjAMvdwUTD_s";
+        String clientId = "801893048902-q8b4mujpfkj31ac6uq37m3p4ms0e9l4s.apps.googleusercontent.com";
+        String clientSecret = "uVpOFGwgbFV59U9GSi74gBZF";
 
         AuthorizationCodeFlow.Builder codeFlowBuilder =
                 new GoogleAuthorizationCodeFlow.Builder(
@@ -40,7 +40,7 @@ public class GoogleCalendarTestThingy {
                 );
         AuthorizationCodeFlow codeFlow = codeFlowBuilder.build();
 
-        String redirectUri = "http://localhost:8080/callback";
+        String redirectUri = "http://localhost:8080/oauth2callback";
         AuthorizationCodeRequestUrl authorizationUrl = codeFlow.newAuthorizationUrl();
         authorizationUrl.setRedirectUri(redirectUri);
         System.out.println("Go to the following address:");
@@ -50,24 +50,44 @@ public class GoogleCalendarTestThingy {
         //to request an authorization token
         System.out.println("What is the 'code' url parameter?");
         String code = new Scanner(System.in).nextLine();
-        AuthorizationCodeTokenRequest tokenRequest = codeFlow.newTokenRequest(code);
-        tokenRequest.setRedirectUri(redirectUri);
-        TokenResponse tokenResponse = tokenRequest.execute();
+        TokenResponse tokenResponse = getTokenResponse(codeFlow, redirectUri, code);
 
         //Now, with the token and user id, we have credentials
         Credential credential = codeFlow.createAndStoreCredential(tokenResponse, null);
 
         //Credentials may be used to initialize http requests
+        Calendar calendar = getCalendar(httpTransport, jsonFactory, credential);
+
+        FreeBusyRequest freeBusyRequest = getFreeBusyRequest();
+
+        Calendar.Freebusy.Query query = calendar.freebusy().query(freeBusyRequest);
+        FreeBusyResponse response = query.execute();
+        System.out.println(response.getKind());
+        for (TimePeriod period : response.getCalendars().get("tshepo.ramatsui@gmail.com").getBusy()) {
+            System.out.println(period);
+
+        }
+    }
+
+    private static Calendar getCalendar(HttpTransport httpTransport, JsonFactory jsonFactory, Credential credential) {
         HttpRequestInitializer initializer = credential;
         //and thus are used to initialize the calendar service
         Calendar.Builder serviceBuilder = new Calendar.Builder(
                 httpTransport, jsonFactory, initializer);
-        serviceBuilder.setApplicationName("My Project");
-        Calendar calendar = serviceBuilder.build();
+        serviceBuilder.setApplicationName("Discovery OBS");
+        return serviceBuilder.build();
+    }
 
+    private static TokenResponse getTokenResponse(AuthorizationCodeFlow codeFlow, String redirectUri, String code) throws IOException {
+        AuthorizationCodeTokenRequest tokenRequest = codeFlow.newTokenRequest(code);
+        tokenRequest.setRedirectUri(redirectUri);
+        return tokenRequest.execute();
+    }
+
+    private static FreeBusyRequest getFreeBusyRequest() throws ParseException {
         FreeBusyRequest freeBusyRequest = new FreeBusyRequest();
         FreeBusyRequestItem freeBusyRequestItem = new FreeBusyRequestItem();
-        freeBusyRequestItem.setId("tramats@thoughtworks.com");
+        freeBusyRequestItem.setId("tshepo.ramatsui@gmail.com");
         List<FreeBusyRequestItem> items = new ArrayList<FreeBusyRequestItem>();
         items.add(freeBusyRequestItem);
         freeBusyRequest.setItems(items);
@@ -77,17 +97,9 @@ public class GoogleCalendarTestThingy {
         Date dtFrom = sdf.parse("2014-10-01");
         DateTime dateMin = new DateTime(dtFrom);
         freeBusyRequest.setTimeMin(dateMin);
-        Date dtTo = sdf.parse("2014-10-20");
+        Date dtTo = sdf.parse("2014-11-05");
         DateTime dateMax = new DateTime(dtTo);
         freeBusyRequest.setTimeMax(dateMax);
-
-        Calendar.Freebusy.Query query = calendar.freebusy().query(freeBusyRequest);
-        FreeBusyResponse response = query.execute();
-        System.out.println(response.getKind());
-        for (TimePeriod period : response.getCalendars().get("tramats@thoughtworks.com").getBusy()) {
-            System.out.println(period);
-
-        }
-
+        return freeBusyRequest;
     }
 }
